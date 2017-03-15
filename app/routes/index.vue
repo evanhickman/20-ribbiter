491b6dfeb87468a6cf9c542333aef65797512164
Evan Hickman <travisevanhickman@gmail.com>
2017-03-15 17:17:08 -0500

    add welcome page

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,11 +1,20 @@
 <template lang="html">
   <div class="">
-
+    <div class="section index-main">
+    <div class="container">
+        <h1 class="heading">Welcome To Ribbiter!</h1>
+      <div class="button-links">
+        <button class="button-link"><a href="#">Register</a></button>
+        <button class="button-link"><a href="#">Login</a></button>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Index',
   data() {
     return {
     };
