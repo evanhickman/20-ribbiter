189a8704a12fc1ccf3b898fcd490f1c851fa8e00
Evan Hickman <travisevanhickman@gmail.com>
2017-03-15 19:19:10 -0500

    v-model and formValues

--- app/routes/register.vue
+++ app/routes/register.vue
@@ -1,21 +1,21 @@
-<template lang="html">
+``<template lang="html">
   <div class="">
     <div class="section register-main">
       <div class="container">
         <div class="card register-card">
           <h1 class="register-card__heading">Create an Account</h1>
-          <form action="submit" class="register-card__form">
+          <form @submit.prevent="submit" class="register-card__form">
             <p class="register-card__username register-card__title">Username</p>
             <p>
-              <input type="text" name="username" class="input">
+              <input type="text" v-model="formValues.username" class="input">
             </p>
             <p class="register-card__email register-card__title">Email</p>
             <p>
-              <input type="text" name="email" class="input">
+              <input type="text" v-model="formValues.email" class="input">
             </p>
             <p class="register-card__password register-card__title">Password</p>
             <p>
-              <input type="text" name="password" class="input">
+              <input type="text" v-model="formValues.password" class="input">
             </p>
             <div class="register-card__buttons">
               <a href="#"><button type="button" name="login" class="button button__login">Login</button></a>
@@ -33,6 +33,11 @@ export default {
   name: 'Register',
   data() {
     return {
+      formValues: {
+        username: '',
+        email: '',
+        password: '',
+      }
     };
   },
 
