8bbec70a26b86f716d715d70e6f812dbaff671de
Evan Hickman <travisevanhickman@gmail.com>
2017-03-15 23:01:36 -0500

    findAll action creator and add posts to data

--- app/routes/ribbits-index.vue
+++ app/routes/ribbits-index.vue
@@ -5,11 +5,19 @@
 </template>
 
 <script>
+import store from '../store.js';
+import postResource from '../../../resources/post';
 export default {
   name: 'ribbits',
   data() {
     return {
+      posts: this.$select('posts'),
     };
+  },
+
+  created() {
+    const { actionCreators: { findAll } } = postResource;
+    store.dispatch(findAll());
   },
 
   methods: {
