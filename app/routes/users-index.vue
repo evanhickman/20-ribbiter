b7d0c7173fd92635f10aecbe5c70bd0e42fa6e57
Evan Hickman <travisevanhickman@gmail.com>
2017-03-15 22:36:05 -0500

    mockup user cards

--- app/routes/users-index.vue
+++ app/routes/users-index.vue
@@ -7,10 +7,17 @@
             <h1>Users</h1>
           </div>
           <div class="grid-vertical__item grid">
-                <img src="http://placehold.it/200" alt="" class="grid__item user-item">
-                <img src="http://placehold.it/200" alt="" class="grid__item user-item">
-                <img src="http://placehold.it/200" alt="" class="grid__item user-item">
-                <img src="http://placehold.it/200" alt="" class="grid__item user-item">
+                <a href="">
+                  <div class="grid__item user-item">
+                    <div class="user-item__left">
+                      <img src="http://placehold.it/100" alt="" class="grid__item-image">
+                    </div>
+                    <div class="user-item__right">
+                      <h2 class="grid__item-name">Bob</h2>
+                      <a href="#" class="grid__item-username">@bob</a>
+                    </div>
+                  </a>
+                </div>
             </div>
           </div>
         </div>
